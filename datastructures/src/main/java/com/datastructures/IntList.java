ad6f5565571543933c2ce42fd57935ca32b2b4f2
Paul Kern <paul.kern@insa-strasbourg.fr>
2023-05-03 18:38:33 +0200

    Added constructors for intlist

--- datastructures/src/main/java/com/datastructures/IntList.java
+++ datastructures/src/main/java/com/datastructures/IntList.java
@@ -1,13 +1,33 @@
 package com.datastructures;
 
 public class IntList {
-    
-    public IntList() {
 
+    private final static int STARTING_SIZE = 32;
+
+    private int[] elements;
+    private int pointer;
+    private int currentSize;
+
+    public IntList() {
+        elements = new int[STARTING_SIZE];
+        currentSize = STARTING_SIZE;
+        pointer = -1;
     }
 
     public IntList(int[] values) {
+        int size = STARTING_SIZE;
+        int valuesLength = values.length;
+        while (size < valuesLength) {
+            size *= 2;
+        }
 
+        elements = new int[size];
+        for (int i = 0; i < valuesLength; i++) {
+            elements[i] = values[i];
+        }
+
+        pointer = valuesLength - 1;
+        currentSize = size;
     }
 
     public void append(int value) {
@@ -31,11 +51,11 @@ public class IntList {
     }
 
     public void remove(int index) {
-        
+
     }
 
     public void extend(IntList list) {
-        
+
     }
 
 }
--- datastructures/src/test/java/com/datastructures/IntListTest.java
+++ datastructures/src/test/java/com/datastructures/IntListTest.java
@@ -10,14 +10,14 @@ public class IntListTest {
     void testBasicOperations() {
         IntList list = new IntList();
         assertEquals(0, list.size(), "new list is not empty");
-        for (int i = 0 ; i < 10 ; i++) {
+        for (int i = 0; i < 10; i++) {
             list.append(i);
         }
         assertEquals(10, list.size(), "list does not have 10 elements");
-        for (int i = 0 ; i < 10 ; i++) {
+        for (int i = 0; i < 10; i++) {
             assertEquals(i, list.get(i), String.format("element %s is not %s", i, i));
         }
-        for (int i = 1 ; i < 10 ; i++) {
+        for (int i = 1; i < 10; i++) {
             assertEquals(9 - i, list.get(-i), String.format("element %s is not %s", -i, 10 - i));
         }
 
@@ -25,8 +25,9 @@ public class IntListTest {
         list.pop(0);
         list.remove(4);
         assertEquals(7, list.size(), "list did not pop 2 elements");
-        for (int i = 1 ; i < 0 ; i++) {
-            if (i == 4) continue;
+        for (int i = 1; i < 0; i++) {
+            if (i == 4)
+                continue;
             assertEquals(i, list.get(i), String.format("element %s is not %s", i, i));
         }
     }
@@ -35,11 +36,11 @@ public class IntListTest {
     void testLargeSize() {
         IntList list = new IntList();
 
-        for (int i = 0 ; i < 10_000_000 ; i++) {
+        for (int i = 0; i < 10_000_000; i++) {
             list.append(i);
         }
 
-        for (int i = 0 ; i < 10_000_000 ; i++) {
+        for (int i = 0; i < 10_000_000; i++) {
             assertEquals(i, list.get(i), String.format("element %s is not %s", i, i));
         }
     }
@@ -49,7 +50,7 @@ public class IntListTest {
         IntList list = new IntList(new int[] {3, 14, 15});
         IntList list2 = new IntList();
 
-        for (int i = 0 ; i < 100 ; i++) {
+        for (int i = 0; i < 100; i++) {
             list2.append(i);
         }
 
@@ -57,7 +58,7 @@ public class IntListTest {
         assertEquals(list.get(0), 3, "element 0 is not 3");
         assertEquals(list.get(1), 14, "element 1 is not 14");
         assertEquals(list.get(2), 15, "element 2 is not 15");
-        for (int i = 0 ; i < 100 ; i++) {
+        for (int i = 0; i < 100; i++) {
             assertEquals(list.get(i + 3), i, String.format("element %s is not %s", i + 3, i));
         }
     }
