8ed84b1ef634233fb050409fa03dc4a609312ec9
Paul Kern <paul.kern@insa-strasbourg.fr>
2023-05-03 18:17:33 +0200

    IntList skeleton

---
+++ datastructures/src/main/java/com/datastructures/IntList.java
@@ -0,0 +1,41 @@
+package com.datastructures;
+
+public class IntList {
+    
+    public IntList() {
+
+    }
+
+    public IntList(int[] values) {
+
+    }
+
+    public void append(int value) {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public int get(int index) {
+        return 0;
+    }
+
+    public int pop() {
+        return pop(-1);
+    }
+
+    public int pop(int index) {
+        return 0;
+    }
+
+    public void removeAt(int index) {
+
+    }
+
+    public void remove(int index) {
+        
+    }
+
+}
